870f880f89d523a6312e30a0b52a3b52c4f5204d
zyguel <11300246@usc.edu.ph>
2023-09-15 22:16:53 +0800

    Finished HourlyEmployee.java

--- EmployeeProject/src/employee/version1/HourlyEmployee.java
+++ EmployeeProject/src/employee/version1/HourlyEmployee.java
@@ -1,5 +1,7 @@
 package employee.version1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 public class HourlyEmployee {
@@ -31,12 +33,12 @@ public class HourlyEmployee {
     }
 
     // Parameterized constructor 2
-    public HourlyEmployee(int empID, String empName, Date empDateHired, Date empBirthDate, float ratePerHour) {
+    public HourlyEmployee(int empID, String empName, Date empDateHired, Date empBirthDate, float totalHoursWorked,float ratePerHour) {
         this.empID = empID;
         this.empName = empName;
         this.empDateHired = empDateHired;
         this.empBirthDate = empBirthDate;
-        this.totalHoursWorked = 0.0f;
+        this.totalHoursWorked = totalHoursWorked;
         this.ratePerHour = ratePerHour;
     }
 
@@ -109,16 +111,84 @@ public class HourlyEmployee {
                 "\nDate Hired: " + empDateHired +
                 "\nBirth Date: " + empBirthDate +
                 "\nTotal Hours Worked: " + totalHoursWorked +
-                "\nRate Per Piece: " + ratePerHour +
+                "\nRate Per Hour: " + ratePerHour +
                 "\nSalary: " + computeSalary();
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	//	Scanner scanner = new Scanner(System.in);
-		HourlyEmployee he = new HourlyEmployee();	
-	//	he.displayinfo();
-		
+        Scanner scanner = new Scanner(System.in);
+        HourlyEmployee[] employees = new HourlyEmployee[10];
+        int numEmployees;
+
+        System.out.println("Enter the number of employees to input: ");
+        numEmployees = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
+        if (numEmployees > employees.length) {
+            System.out.println("Number of employees exceeds the maximum limit.");
+            return;
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+        // Input employee data
+        for (int i = 0; i < numEmployees; i++) {
+            System.out.println("Enter data for Employee #" + (i + 1));
+
+            // Employee ID
+            System.out.print("Employee ID: ");
+            int empID = scanner.nextInt();
+          
+            scanner.nextLine(); // Consume the newline character
+
+            // Employee Name
+            System.out.print("Employee Name: ");
+            String empName = scanner.nextLine();
+           
+
+            // Date Hired (assuming date format is "dd-MM-yyyy")
+            System.out.print("Date Hired (dd-MM-yyyy): ");
+            String dateHiredStr = scanner.nextLine();
+            Date empDateHired = null;
+            try {
+                empDateHired = dateFormat.parse(dateHiredStr);
+            } catch (ParseException e) {
+                System.out.println("Invalid date format. Please use dd-MM-yyyy.");
+                return;
+            }
+       
+
+            // Birth Date (assuming date format is "dd-MM-yyyy")
+            System.out.print("Birth Date (dd-MM-yyyy): ");
+            String birthDateStr = scanner.nextLine();
+            Date empBirthDate = null;
+            try {
+                empBirthDate = dateFormat.parse(birthDateStr);
+            } catch (ParseException e) {
+                System.out.println("Invalid date format. Please use dd-MM-yyyy.");
+                return;
+            }
+            // Total Hours Worked
+            System.out.print("Total Hours Worked: ");
+            float totalHoursWorked = scanner.nextFloat();
+
+
+            // Rate Per Hour
+            System.out.print("Rate Per Hour: ");
+            float ratePerHour = scanner.nextFloat();
+         
+            // Create an employee object with the entered data
+            employees[i] = new HourlyEmployee(empID, empName, empDateHired, empBirthDate, totalHoursWorked, ratePerHour);
+            
+        }
+
+        // Display information for all employees
+       for (int i = 0; i < numEmployees; i++) {
+            System.out.println("Employee #" + (i + 1));
+            System.out.println(employees[i].toString());
+        }
+     
 	}
 
 }
