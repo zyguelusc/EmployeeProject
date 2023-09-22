80c94f46b8e8982c3c59fcc9c02ac3060ef81eb2
zyguel <11300246@usc.edu.ph>
2023-09-22 21:42:15 +0800

    Updated files since version 1 main got corrupted. Version 2 in development

---
+++ EmployeeProject/src/employee/version2/Employee.java
@@ -0,0 +1,8 @@
+package employee.version2;
+
+public class Employee {
+   
+    public static void main (String[] args){
+        System.out.println("Hello OOP!");
+    }
+}
---
+++ EmployeeProject/src/employee/version2/Main.java
@@ -0,0 +1,25 @@
+package employee.version2;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp1 = new Employee();
+		emp1.setEmpID(1);
+		emp1.setEmpName("John");
+		emp1.setEmpDateHired("2018-01-01");
+		emp1.setEmpBirthDate("1990-01-01");
+		emp1.displayInfo();
+			
+
+	
+	}
+
+}
